ce8f6e69398957395c3e834502622060eba93816
Unknown <99053978@mydavinci.nl>
2019-02-01 12:49:52 +0100

    Commit 01/02/19
    Mobile size responsive finished

--- js/js.js
+++ js/js.js
@@ -24,3 +24,21 @@ function showDivs(n) {
   x[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " w3-white";
 }
+
+function topButton(){
+  var topNav = document.getElementById("upperbar");
+  if (topNav.className == "") {
+    topNav.className = "active";
+
+  }else
+    topNav.className = "";
+}
+
+function botButton(){
+  var botNav = document.getElementById("lowerbar");
+  if (botNav.className == "") {
+    botNav.className = "active";
+
+  }else
+    botNav.className = "";
+}
